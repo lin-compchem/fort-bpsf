d2d69010308470dc55807f74329b89c6a7d76d22
Adam Duster <adamduster@outlook.com>
2020-04-21 19:02:29 -0600

    accidentally deleted parameters file...

---
+++ src/parameters.h
@@ -0,0 +1,16 @@
+/*
+#define int8
+#ifdef int8
+#define ANUMKIND 8
+#define NATMKIND 8
+#else
+#define ANUMKIND 1
+#define NATMKIND 2
+#endif
+*/
+#define ANUMKIND 1
+#define NATMKIND 2
+#define MAXATOM 99 
+#define MAXBAS 150
+#define NUMELS 2
+#define DEBUG
