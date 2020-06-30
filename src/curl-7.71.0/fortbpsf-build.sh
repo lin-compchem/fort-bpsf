450fb8289090cf5af94a3f3c4499d07b82c3d1ab
Adam Duster <adamduster@outlook.com>
2020-06-30 02:46:22 -0600

    Added copies and instructions of the required hdf5, libcurl, and rapidjson libraries

---
+++ src/curl-7.71.0/fortbpsf-build.sh
@@ -0,0 +1,8 @@
+./configure --prefix=`readlink -f ../../
+make -j8
+cd lib
+make install
+cd ../
+cd include
+make install
+cd ../
---
+++ src/hdf5-1.12.0/fortbpsf-build.sh
@@ -0,0 +1,3 @@
+set -x
+./configure --enable-fortran --prefix=`readlink -f ../../` --enable-tools=no --enable-tests=no
+make -j16
